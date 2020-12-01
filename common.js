86723d7155e16881892baaba9dfd294170fe22c0
Liao <2429463592@qq.com>
2020-12-01 15:18:25 +0800

    创建数据库操作模块

---
+++ common.js
@@ -0,0 +1,43 @@
+/*
+  封装通用的数据库操作  装数据库操作模块，基于Promise方式获取数据库结果
+*/
+function operateDb (sql, params) {
+    // 返回Promise实例对象
+    return new Promise(function (resolve, reject) {
+      // 1) 加载 MySQL 模块
+      var mysql = require('mysql')
+      // 2) 创建 MySQL 连接对象
+      var cn = mysql.createConnection({
+        // 数据库所在计算机的IP地址或者域名
+        host: 'localhost',
+        // 数据库的端口
+        port: 3306,
+        // 数据库名称
+        database: '117',
+        // 登录数据库的账号名称
+        user: 'root',
+        // 登录数据库的账号密码
+        password: 'adm123'
+      })
+      // 3) 连接 MySQL 服务器
+      cn.connect()
+      // 4) 执行SQL语句           
+      // 查询返回的结果是什么？数组
+      // 增删改操作的结果是什么？对象（affectedRows）
+      // params  查询条件  占位符 ? 
+      cn.query(sql, params, function (err, result) {
+        if (err) {
+          return reject('数据库操作失败')
+        }
+        // 数据库的操作是异步的（只有获取到异步结果后，回调函数才会执行）
+        resolve(result)
+      })
+      // 5) 关闭链接  
+      cn.end() 
+    })
+  }
+  
+  module.exports = {
+    operateDb: operateDb
+  }
+  
--- router/login-router.js
+++ router/login-router.js
@@ -2,13 +2,32 @@
 const express = require('express')
 const path = require('path')
 
+// 导入数据库通用模块
+const db = require(path.join(__dirname, '../common.js'))
+
+
 //拆分路由模块,可以将路由添加到router对象上
 // 在index.js文件中通过app.use方法把router中的路由配置到全局
 const router = express.Router()
 
-router.get('/data', (req, res) => {
-    res.end('data')
-})
-
+// 测试数据库接口
+router.get('/test', async (req, res) => {
+    //查询表单数据
+    let sql = 'select * from myuser'
+    //返回的是promise实例对象,sql,上一句查询条件,null是占位符,查询条件
+    let ret = await db.operateDb(sql, null)
+    if (ret && ret.length > 0) {
+      res.json({
+        status: 0,
+        message: '查询数据成功',
+        data: ret
+      })
+    } else {
+      res.json({
+        status: 1,
+        message: '查询数据失败'
+      })
+    }
+  })
 //导出路由
 module.exports = router
